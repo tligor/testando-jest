48b960b31d308dc89dd6893b271576fb3b15d246
tligor <santanna.ig@hotmail.com>
2025-01-29 18:21:09 -0300

    Aprendendo a realizar testes com Jest

---
+++ testes-jest/src/App.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('Testes para o componente principal', () => {
+  test('Deve renderizar corretamente', () => {
+    render(<App />);
+    expect(screen.getByText('cadastrar')).toBeInTheDocument()
+  });
+});
